db536d7e021ab31dc74bcb635db38f15c1790db8
Takeshi Kawajiri <kzilet@gmail.com>
2012-02-29 02:35:20 +0900

    refactoring

--- js/map.js
+++ js/map.js
@@ -1,20 +1,14 @@
 $(function(){
-  var map,
-      latlng,
-      centerMarker,
-      geocoder;
-
   /*********************************
    * 地図情報の初期化
    */
-  var initialize = (function () {
-    var opts;
+  var centerMarker = (function () {
 
     // 中心地の軽度緯度
-    latlng = new google.maps.LatLng(35.607002,139.749107),
+    var latlng = new google.maps.LatLng(35.607002,139.749107);
 
     // 地図の設定
-    mapopts = {
+    var mapopts = {
       zoom: 11,
       center: latlng,
       mapTypeId: google.maps.MapTypeId.ROADMAP,
@@ -23,13 +17,13 @@ $(function(){
       }
     };
 
-    map = new google.maps.Map(
+    var map = new google.maps.Map(
       document.getElementById('map_canvas'),
       mapopts
     );
 
     // 中心地のマーカー設定
-    centerMarker = new google.maps.Marker({
+    return new google.maps.Marker({
       map: map,
       position: latlng
     });
@@ -38,15 +32,16 @@ $(function(){
   /*********************************
    * 住所から地図を探す
    */
-  var search_text = $('#search_text').textinput();
-  search_text.bind('change', function (event, ui) {
-    var address = this.value;
-    geocoder = new google.maps.Geocoder();
+  $('#search_text').textinput().bind('change', function (event, ui) {
+    var address = $(this).val();
+    var geocoder = new google.maps.Geocoder();
 
     geocoder.geocode({'address': address}, function (results, status){
+      var location;
       if (status == google.maps.GeocoderStatus.OK) {
-        map.setCenter(results[0].geometry.location);
-        centerMarker.setPosition(results[0].geometry.location)
+        location = results[0].geometry.location;
+        centerMarker.map.setCenter(location);
+        centerMarker.setPosition(location);
       } else {
         alert("Geocode was not successful for the following reason: " + status);
       }
